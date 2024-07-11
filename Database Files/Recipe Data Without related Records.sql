4f06ee73d86ffc61fd88c1a4782d5a0d326f76e1
rifinkelstein <rivkafinkelstein823@gmail.com>
2024-07-11 16:20:08 +0300

    recipe data withou related records

--- Database Files/Recipe Data Without related Records.sql
+++ Database Files/Recipe Data Without related Records.sql
@@ -2,9 +2,9 @@ use HeartyHealthDB
 
 
 insert Recipe(UsersID, CuisineID, RecipeName, calories, DraftedDate, PublishedDate, ArchivedDate)
-SELECT (Select u.usersID from users U WHERE UserName = 'MStephans'), (select C.CuisineID from Cuisine C where cuisinename= 'American'), 'CC Muffins ', 300, '01-20-2012', '01-11-2016', '12-03-2021'
-UNION SELECT (Select u.usersID from users U WHERE UserName = 'MStephans'), (select C.CuisineID from Cuisine C where cuisinename= 'American'), 'Cinaman Butter Muffins ', 300, '01-20-2012', '01-11-2016', '12-03-2021'
-UNION SELECT (Select u.usersID from users U WHERE UserName = 'MStephans'), (select C.CuisineID from Cuisine C where cuisinename= 'American'), 'PanutButter Muffins ', 300, '01-20-2012', '01-11-2016', '12-03-2021'
+SELECT (Select u.usersID from users U WHERE UserName = 'MStephans'), (select C.CuisineID from Cuisine C where cuisinename= 'American'), 'Rocky Road Muffins ', 300, '01-20-2012', '01-11-2016', '12-03-2021'
+UNION SELECT (Select u.usersID from users U WHERE UserName = 'MStephans'), (select C.CuisineID from Cuisine C where cuisinename= 'American'), 'Peanut Butter Muffins ', 300, '01-20-2012', '01-11-2016', '12-03-2021'
+UNION SELECT (Select u.usersID from users U WHERE UserName = 'MStephans'), (select C.CuisineID from Cuisine C where cuisinename= 'American'), 'Vanilla Muffins ', 300, '01-20-2012', '01-11-2016', '12-03-2021'
 
 
 
