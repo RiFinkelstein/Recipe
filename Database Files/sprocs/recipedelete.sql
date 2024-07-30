2c88ed2b1c9941614502a320b7fe8125529b95e4
rifinkelstein <rivkafinkelstein823@gmail.com>
2024-07-30 10:32:52 +0300

    update recipedelete sproc

--- Database Files/sprocs/recipedelete.sql
+++ Database Files/sprocs/recipedelete.sql
@@ -1,8 +1,18 @@
 CREATE or ALTER PROCEDURE dbo.RecipeDelete(
-    @recipeid INT    
+    @recipeid INT,
+    @message VARCHAR(500) = '' OUTPUT
 )
 as 
 begin 
+    DECLARE @return int =0
+if exists (SELECT* from recipe r where r.RecipeID=@recipeid and   (r.Status = 'drafted' or DATEDIFF(day, r.ArchivedDate, GETDATE())>30))
+
+BEGIN
+  SELECT @return =1, 
+  @message= 'cannot delete recipe that is drafted or the recipe has been archived more than 30 days ago'
+  goto finished
+END
+
 begin try
 begin tran
     delete ri from RecipeIngredient ri where ri.recipeID= @recipeid
@@ -15,6 +25,8 @@ begin tran
 	rollback;
 	throw
 end catch
+    finished: 
+    return @return
 
 END
 go
--- Database Files/test/query.sql
+++ Database Files/test/query.sql
@@ -9,3 +9,5 @@ JOIN CookbookRecipe cbr
 ON r.RecipeID = cbr.RecipeID
 where r.Status = 'drafted' 
 or DATEDIFF(day, r.ArchivedDate, GETDATE())>30
+
+select * from recipe where RecipeID=5
