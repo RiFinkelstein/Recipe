adff1355aacf6e149f83e925d8e6669ff98d5c09
rifinkelstein <rivkafinkelstein823@gmail.com>
2024-08-08 11:26:45 +0300

    fixed functions

--- Database Files/functions/RecipeInfo.sql
+++ Database Files/functions/RecipeInfo.sql
@@ -9,13 +9,7 @@ begin
     from recipe r 
     join Cuisine c
     on c.CuisineID = r.CuisineID
-    join RecipeIngredient RI
-    on RI.RecipeID = r.RecipeID
-    join Directions d 
-    on d.RecipeID= r.RecipeID
     where r.RecipeID= @recipeid
-    GROUP by r.RecipeID, r.RecipeName, c.CuisineName
-
 RETURN @value
 end 
 go
