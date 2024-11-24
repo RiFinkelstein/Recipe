f0295efae83f60cb9004bd1dc1937378630bf4ec
rifinkelstein <rivkafinkelstein823@gmail.com>
2024-11-24 19:40:42 +0200

    sprocs

--- Database Files/sprocs/RecipeIngredientupdate.sql
+++ Database Files/sprocs/RecipeIngredientupdate.sql
@@ -29,4 +29,3 @@ begin
 	return @return
 end
 go
-
---
+++ Database Files/sprocs/RecipeStepsUpdate.sql
@@ -0,0 +1,33 @@
+create or alter procedure dbo.RecipeStepsUpdate(
+	@directionsID int  output,
+	@recipeId int ,
+    @Direction VARCHAR(1000),
+    @step int,
+	@Message varchar(500) = ''  output
+)
+as
+begin
+	declare @return int = 0
+
+	select @directionsID = isnull(@directionsID,0)
+
+	if @directionsID = 0
+	begin
+		insert Directions(RecipeID, Direction, StepNumber)
+		values(@recipeId, @Direction, @step)
+
+		select @directionsID= scope_identity()
+	end
+	else
+	begin
+		update Directions
+		set
+			recipeId = @recipeId 
+			--directionsID = @directionsID
+		where directionsID = @directionsID
+	end
+
+	return @return
+end
+go
+
