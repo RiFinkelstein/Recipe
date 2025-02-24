44f5445c0b004fc3a811bb8389b5fb77fbe9d7cf
RiFinkelstein <116811257+RiFinkelstein@users.noreply.github.com>
2025-02-24 19:12:57 +0200

    Update CookbookRecipeDelete.sql

--- Database Files/sprocs/CookbookRecipeDelete.sql
+++ Database Files/sprocs/CookbookRecipeDelete.sql
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 --AS Sproc name should be proper cased
-CREATE or alter PROCEDURE dbo.cookbookrecipeDelete(
-=======
 CREATE or alter PROCEDURE dbo.CookbookRecipeDelete(
->>>>>>> ce296b26ad62d933db5305df0d18be524562a86d
     @CookbookRecipeID int =0, 
     @Message varchar(500) = ''  output
 )
