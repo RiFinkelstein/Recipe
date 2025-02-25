4289f33fb8bc57af8e5820f69457297228247c58
rifinkelstein <rivkafinkelstein823@gmail.com>
2025-02-25 19:40:33 +0200

    fixed cookbookautocreate sproc to be cleaner

--- Database Files/sprocs/CookbookAutoCreate.sql
+++ Database Files/sprocs/CookbookAutoCreate.sql
@@ -5,22 +5,10 @@ CREATE OR ALTER PROCEDURE dbo.CookbookAutoCreate(
 AS
 BEGIN
 --AS There is no reason why this code has to be all broken up using variables, this can be done simply in 2 insert statements (the same way it was done in maintenance tasks.)    
-    DECLARE @CookbookName VARCHAR(100)= (
-        select 'Recipes by ' + U.UsersFirstName + ' ' + U.UsersLastName
-        from users u
-        where UsersID= @UsersID
-    )
-    --check if cookbook already exists
-    IF EXISTS (SELECT 1 FROM Cookbook WHERE CookbookName = @CookbookName AND UsersID = @UsersID)
-    BEGIN
-    SET @Message = 'A cookbook with this name already exists for this user.';
-    RETURN;
-    END
-
     -- Insert into Cookbook table
     INSERT Cookbook (UsersID, CookbookName, Price, DateCreated, Active)
     VALUES (@UsersID, 
-    @CookbookName, 
+    (select 'Recipes by ' + U.UsersFirstName + ' ' + U.UsersLastName from users u where UsersID= @UsersID), 
     (select count(*) * 1.33 from recipe r where r.UsersID= @UsersID), 
     GETDATE()
     , 1);
