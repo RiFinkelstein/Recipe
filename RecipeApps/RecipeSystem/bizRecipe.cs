50ecec129da07f1253c9b26890aab037641ef214
rifinkelstein <rivkafinkelstein823@gmail.com>
2025-06-27 02:39:32 -0400

    added properties

--- RecipeApps/RecipeSystem/bizRecipe.cs
+++ RecipeApps/RecipeSystem/bizRecipe.cs
@@ -9,9 +9,120 @@ namespace RecipeSystem
 {
     public class bizRecipe : bizObject
     {
-        public bizRecipe() : base("Recipe")
+        public bizRecipe() { }
+        private int _recipeid;
+        private int _cuisineid;
+        private int _usersid;
+        private string _recipename;
+        private int _calories;
+        private DateTime _drafteddate;
+        private DateTime _publisheddate;
+        private DateTime _archiveddate;
+
+
+        public int RecipeID
+        {
+            get { return _recipeid; }
+            set
+            {
+                if (_recipeid != value)
+                {
+                    _recipeid = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public int CuisineID
+        {
+            get { return _cuisineid; }
+            set
+            {
+                if (_cuisineid != value)
+                {
+                    _cuisineid = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public int UsersID
+        {
+            get { return _usersid; }
+            set
+            {
+                if (_usersid != value)
+                {
+                    _usersid = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public string RecipeName
+        {
+            get { return _recipename; }
+            set
+            {
+                if (_recipename != value)
+                {
+                    _recipename = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        private int Calories
         {
 
+            get { return _calories; }
+            set
+            {
+                if (_calories != value)
+                {
+                    _calories = value;
+                    InvokePropertyChanged();
+                }
+            }
         }
+
+        public DateTime DraftedDate
+        {
+            get { return _drafteddate; }
+            set
+            {
+                if (_drafteddate != value)
+                {
+                    _drafteddate = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
+        public DateTime PublishedDate
+        {
+            get { return _publisheddate; }
+            set
+            {
+                if (_publisheddate != value)
+                {
+                    _publisheddate = value;
+                    InvokePropertyChanged();
+                }
+            }
+        } 
+        public DateTime ArchivedDate
+        {
+            get { return _archiveddate; }
+            set
+            {
+                if (_archiveddate != value)
+                {
+                    _archiveddate = value;
+                    InvokePropertyChanged();
+                }
+            }
+        }
+
     }
 }
